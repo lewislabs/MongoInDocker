ee86debb29f7c7cb6ba97a910d2d4932a6931e1d
Jon Evans <glitch100@gmail.com>
2019-01-04 10:41:46 +0000

    Added C# light client for test

---
+++ MongoLightweight/Client/Person.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Client
+{
+    [BsonIgnoreExtraElements]
+    public class Person
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        [BsonElement("name")]
+        public string Name { get; set; }
+        [BsonElement("age")]
+        public int Age { get; set; }
+    }
+}
