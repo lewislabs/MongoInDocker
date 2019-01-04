5e93822ebf87f2b8b5baa4d4daee58bc42473212
Jon Evans <glitch100@gmail.com>
2019-01-04 10:29:24 +0000

    Adjusted compose file, and scripts

---
+++ scripts/router/add_shard.js
@@ -0,0 +1,1 @@
+sh.addShard("rs1/mongoindocker_shard_rs1_3_1:27018,mongoindocker_shard_rs1_2_1:27018,mongoindocker_shard_rs1_2_1:27018" )
--- scripts/shard/initiate_shards.js
+++ scripts/shard/initiate_shards.js
@@ -1,8 +1,9 @@
-var config = { 
-    _id:"rs1", 
+var config = {
+    _id:"rs1",
     members:[
         { _id : 0, host : "mongoindocker_shard_rs1_1_1:27018" },
         { _id : 1, host : "mongoindocker_shard_rs1_2_1:27018" },
         { _id : 2, host : "mongoindocker_shard_rs1_3_1:27018" }
     ]};
+
 rs.initiate(config);
